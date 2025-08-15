e5f9d983f46565ba9a4664b47e3626145829ed7d
lukkaz2709 <lmiguez2709@gmail.com>
2025-08-15 12:54:56 -0300

    VivaSegura project update!

--- vivaSegura/src/screens/FormScrren.tsx
+++ vivaSegura/src/screens/FormScrren.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, ScrollView, ImageBackground } 
+from 'react-native';
 import * as Location from 'expo-location';
 
 export default function FormScreen( {navigation} : {navigation: any} ) {
@@ -22,7 +23,12 @@ export default function FormScreen( {navigation} : {navigation: any} ) {
   }, []);
 
   return (
+    
     <ScrollView contentContainerStyle={styles.container}>
+        <ImageBackground 
+        source={require('../assets/vivasegura-imagem-fundo.png')}
+        style={styles.background}
+    >
       <Text style={styles.title}>Formulário de Segurança</Text>
 
       <TextInput
@@ -57,29 +63,36 @@ export default function FormScreen( {navigation} : {navigation: any} ) {
 
       <Button title="Enviar" onPress={() => console.log(formData)} color="#1569e7" />
       <Button title="Voltar" onPress={() => navigation.goBack()} color="#999" />
+        </ImageBackground>
     </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#fff'
+    background: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
   },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    textAlign: 'center'
+    container: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        padding: 20,
+        backgroundColor: '#fff'
   },
-  input: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    marginBottom: 10
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center'
+  },
+    input: {
+        backgroundColor: '#fff',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        marginBottom: 10
   }
 });
